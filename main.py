93c013f93084ed1054df680e1ae60003daaa1ea3
12alain <kiemdealain02@gmail.com>
2023-08-24 14:17:13 +0000

    data cleaning . #2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-from script import download
+from script import download ,data_cleaned
 url='https://raw.githubusercontent.com/12alain/python/master/data/billets.csv'
 download(url)
+data_cleaned()
--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 import os
 
 def download(url,dataset='data_file.csv'):
@@ -8,4 +9,20 @@ def download(url,dataset='data_file.csv'):
     with open(file_path,'wb') as file:
 
         file.write(response.content)
-     
+
+def data_cleaned():
+  
+  datas=pd.read_csv('./data/raw/data_file.csv')
+  # traitement des valeurs manquantes par la moyenne
+  for i in datas.columns:
+    p=datas[i].isnull().sum()
+    if p!=0:
+      if datas[i].dtype in [int, float]:
+         datas[i].fillna(value=datas[i].mean(),inplace=True)
+      else :
+        mode_value = datas[i].mode()[0]
+        datas[i].fillna(value=mode_value, inplace=True)
+    # Traitement des valeurs dupliquées
+    datas.drop_duplicates(inplace=True)
+    datas.to_csv('./data/cleaned/data_cleaned.csv')
+    return datas 
