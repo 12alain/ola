0b67f311bf34e53a80cbcb6e7b1d28b9eb73a546
12alain <kiemdealain02@gmail.com>
2023-08-24 01:30:49 +0000

    download update

--- script.py
+++ script.py
@@ -2,8 +2,9 @@ import requests
 import os
 
 def download(url,dataset='data_file.csv'):
+    os.makedirs('data/raw', exist_ok=True)
     response=requests.get(url)
-    file_path = os.path.join('.data/raw', dataset)
+    file_path = os.path.join('data/raw', dataset)
     with open(file_path,'wb') as file:
 
         file.write(response.content)
